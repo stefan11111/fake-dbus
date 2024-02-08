8fd6e5c3f7ecf1b7cbe588a57962d7a18909d239
stefan11111 <stefan11111@shitposting.expert>
2024-02-08 20:42:54 +0200

    change void to void*

--- libdbus-1.c
+++ libdbus-1.c
@@ -1,115 +1,115 @@
-void dbus_message_iter_close_container() {}
-void dbus_message_iter_open_container() {}
-void dbus_message_iter_append_basic() {}
-void dbus_message_iter_recurse() {}
-void dbus_message_iter_get_arg_type() {}
-void dbus_message_iter_get_basic() {}
-void dbus_message_iter_init () {}
-void dbus_bus_get() {}
-void dbus_message_new() {}
-void dbus_message_iter_next() {}
-void dbus_message_iter_init_append () {}
-void dbus_message_unref() {}
-void dbus_message_get_signature() {}
-void dbus_message_get_args () {}
-void dbus_connection_close() {}
-void dbus_connection_unref() {}
-void dbus_connection_get_dispatch_status() {}
-void dbus_connection_ref() {}
-void dbus_connection_dispatch() {}
-void dbus_watch_set_data() {}
-void dbus_watch_handle() {}
-void dbus_timeout_set_data() {}
-void dbus_timeout_handle() {}
-void dbus_watch_get_data() {}
-void dbus_timeout_get_enabled() {}
-void dbus_timeout_get_interval() {}
-void dbus_timeout_get_data() {}
-void dbus_watch_get_flags() {}
-void dbus_watch_get_unix_fd() {}
-void dbus_watch_get_enabled() {}
-void dbus_message_new_method_call() {}
-void dbus_message_append_args() {}
-void dbus_error_init() {}
-void dbus_connection_send_with_reply_and_block() {}
-void dbus_error_is_set() {}
-void dbus_error_free() {}
-void dbus_message_get_path() {}
-void dbus_message_new_method_return() {}
-void dbus_connection_send() {}
-void dbus_connection_get_data() {}
-void dbus_connection_set_data() {}
-void dbus_connection_set_watch_functions() {}
-void dbus_connection_set_timeout_functions() {}
-void dbus_connection_set_wakeup_main_function() {}
-void dbus_server_allocate_data_slot() {}
-void dbus_server_get_data() {}
-void dbus_server_set_watch_functions() {}
-void dbus_server_set_timeout_functions() {}
-void dbus_message_new_signal() {}
-void dbus_bus_remove_match() {}
-void dbus_message_get_type() {}
-void dbus_connection_allocate_data_slot() {}
-void dbus_server_set_data() {}
-void dbus_bus_add_match() {}
-void dbus_message_get_interface() {}
-void dbus_message_get_sender() {}
-void dbus_message_get_member() {}
-void dbus_pending_call_unref() {}
-void dbus_pending_call_steal_reply() {}
-void dbus_message_is_method_call() {}
-void dbus_message_is_signal() {}
-void dbus_connection_free_data_slot() {}
-void dbus_connection_send_with_reply() {}
-void dbus_pending_call_set_notify() {}
-void dbus_connection_get_is_connected() {}
-void dbus_connection_open_private() {}
-void dbus_bus_register() {}
-void dbus_bus_get_unique_name() {}
-void dbus_message_get_destination() {}
-void dbus_pending_call_ref() {}
-void dbus_connection_read_write_dispatch() {}
-void dbus_pending_call_cancel() {}
-void dbus_set_error_const() {}
-void dbus_set_error() {}
-void dbus_connection_add_filter() {}
-void dbus_bus_request_name() {}
-void dbus_message_iter_get_signature() {}
-void dbus_free() {}
-void dbus_message_iter_get_fixed_array() {}
-void dbus_message_get_error_name() {}
-void dbus_message_ref() {}
-void dbus_message_new_error() {}
-void dbus_connection_flush() {}
-void dbus_connection_register_fallback() {}
-void dbus_connection_register_object_path() {}
-void dbus_message_set_no_reply() {}
-void dbus_message_set_destination() {}
-void dbus_connection_set_unix_user_function() {}
-void dbus_address_escape_value() {}
-void dbus_server_listen() {}
-void dbus_server_set_new_connection_function() {}
-void dbus_connection_remove_filter() {}
-void dbus_bus_release_name() {}
-void dbus_connection_unregister_object_path() {}
-void dbus_connection_open() {}
-void dbus_bus_get_private() {}
-void dbus_connection_set_exit_on_disconnect () {}
-void dbus_shutdown() {}
-void dbus_signature_validate() {}
-void dbus_signature_iter_next() {}
-void dbus_message_iter_abandon_container() {}
-void dbus_pending_call_block() {}
-void dbus_connection_list_registered() {}
-void dbus_threads_init_default() {}
-void dbus_connection_try_register_object_path() {}
-void dbus_message_get_no_reply() {}
-void dbus_connection_get_object_path_data() {}
-void dbus_set_error_from_message() {}
-void dbus_message_set_path() {}
-void dbus_signature_iter_init() {}
-void dbus_signature_iter_recurse() {}
-void dbus_signature_iter_get_current_type() {}
-void dbus_message_iter_append_fixed_array() {}
-void dbus_message_set_interface() {}
-void dbus_free_string_array() {}
+void* dbus_message_iter_close_container() { return 0; }
+void* dbus_message_iter_open_container() { return 0; }
+void* dbus_message_iter_append_basic() { return 0; }
+void* dbus_message_iter_recurse() { return 0; }
+void* dbus_message_iter_get_arg_type() { return 0; }
+void* dbus_message_iter_get_basic() { return 0; }
+void* dbus_message_iter_init () { return 0; }
+void* dbus_bus_get() { return 0; }
+void* dbus_message_new() { return 0; }
+void* dbus_message_iter_next() { return 0; }
+void* dbus_message_iter_init_append () { return 0; }
+void* dbus_message_unref() { return 0; }
+void* dbus_message_get_signature() { return 0; }
+void* dbus_message_get_args () { return 0; }
+void* dbus_connection_close() { return 0; }
+void* dbus_connection_unref() { return 0; }
+void* dbus_connection_get_dispatch_status() { return 0; }
+void* dbus_connection_ref() { return 0; }
+void* dbus_connection_dispatch() { return 0; }
+void* dbus_watch_set_data() { return 0; }
+void* dbus_watch_handle() { return 0; }
+void* dbus_timeout_set_data() { return 0; }
+void* dbus_timeout_handle() { return 0; }
+void* dbus_watch_get_data() { return 0; }
+void* dbus_timeout_get_enabled() { return 0; }
+void* dbus_timeout_get_interval() { return 0; }
+void* dbus_timeout_get_data() { return 0; }
+void* dbus_watch_get_flags() { return 0; }
+void* dbus_watch_get_unix_fd() { return 0; }
+void* dbus_watch_get_enabled() { return 0; }
+void* dbus_message_new_method_call() { return 0; }
+void* dbus_message_append_args() { return 0; }
+void* dbus_error_init() { return 0; }
+void* dbus_connection_send_with_reply_and_block() { return 0; }
+void* dbus_error_is_set() { return 0; }
+void* dbus_error_free() { return 0; }
+void* dbus_message_get_path() { return 0; }
+void* dbus_message_new_method_return() { return 0; }
+void* dbus_connection_send() { return 0; }
+void* dbus_connection_get_data() { return 0; }
+void* dbus_connection_set_data() { return 0; }
+void* dbus_connection_set_watch_functions() { return 0; }
+void* dbus_connection_set_timeout_functions() { return 0; }
+void* dbus_connection_set_wakeup_main_function() { return 0; }
+void* dbus_server_allocate_data_slot() { return 0; }
+void* dbus_server_get_data() { return 0; }
+void* dbus_server_set_watch_functions() { return 0; }
+void* dbus_server_set_timeout_functions() { return 0; }
+void* dbus_message_new_signal() { return 0; }
+void* dbus_bus_remove_match() { return 0; }
+void* dbus_message_get_type() { return 0; }
+void* dbus_connection_allocate_data_slot() { return 0; }
+void* dbus_server_set_data() { return 0; }
+void* dbus_bus_add_match() { return 0; }
+void* dbus_message_get_interface() { return 0; }
+void* dbus_message_get_sender() { return 0; }
+void* dbus_message_get_member() { return 0; }
+void* dbus_pending_call_unref() { return 0; }
+void* dbus_pending_call_steal_reply() { return 0; }
+void* dbus_message_is_method_call() { return 0; }
+void* dbus_message_is_signal() { return 0; }
+void* dbus_connection_free_data_slot() { return 0; }
+void* dbus_connection_send_with_reply() { return 0; }
+void* dbus_pending_call_set_notify() { return 0; }
+void* dbus_connection_get_is_connected() { return 0; }
+void* dbus_connection_open_private() { return 0; }
+void* dbus_bus_register() { return 0; }
+void* dbus_bus_get_unique_name() { return 0; }
+void* dbus_message_get_destination() { return 0; }
+void* dbus_pending_call_ref() { return 0; }
+void* dbus_connection_read_write_dispatch() { return 0; }
+void* dbus_pending_call_cancel() { return 0; }
+void* dbus_set_error_const() { return 0; }
+void* dbus_set_error() { return 0; }
+void* dbus_connection_add_filter() { return 0; }
+void* dbus_bus_request_name() { return 0; }
+void* dbus_message_iter_get_signature() { return 0; }
+void* dbus_free() { return 0; }
+void* dbus_message_iter_get_fixed_array() { return 0; }
+void* dbus_message_get_error_name() { return 0; }
+void* dbus_message_ref() { return 0; }
+void* dbus_message_new_error() { return 0; }
+void* dbus_connection_flush() { return 0; }
+void* dbus_connection_register_fallback() { return 0; }
+void* dbus_connection_register_object_path() { return 0; }
+void* dbus_message_set_no_reply() { return 0; }
+void* dbus_message_set_destination() { return 0; }
+void* dbus_connection_set_unix_user_function() { return 0; }
+void* dbus_address_escape_value() { return 0; }
+void* dbus_server_listen() { return 0; }
+void* dbus_server_set_new_connection_function() { return 0; }
+void* dbus_connection_remove_filter() { return 0; }
+void* dbus_bus_release_name() { return 0; }
+void* dbus_connection_unregister_object_path() { return 0; }
+void* dbus_connection_open() { return 0; }
+void* dbus_bus_get_private() { return 0; }
+void* dbus_connection_set_exit_on_disconnect () { return 0; }
+void* dbus_shutdown() { return 0; }
+void* dbus_signature_validate() { return 0; }
+void* dbus_signature_iter_next() { return 0; }
+void* dbus_message_iter_abandon_container() { return 0; }
+void* dbus_pending_call_block() { return 0; }
+void* dbus_connection_list_registered() { return 0; }
+void* dbus_threads_init_default() { return 0; }
+void* dbus_connection_try_register_object_path() { return 0; }
+void* dbus_message_get_no_reply() { return 0; }
+void* dbus_connection_get_object_path_data() { return 0; }
+void* dbus_set_error_from_message() { return 0; }
+void* dbus_message_set_path() { return 0; }
+void* dbus_signature_iter_init() { return 0; }
+void* dbus_signature_iter_recurse() { return 0; }
+void* dbus_signature_iter_get_current_type() { return 0; }
+void* dbus_message_iter_append_fixed_array() { return 0; }
+void* dbus_message_set_interface() { return 0; }
+void* dbus_free_string_array() { return 0; }
